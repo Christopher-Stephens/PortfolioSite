1cde309322cdf395762e6a2e16b9f036e0360014
Christopher Stephens <stephchw@iu.edu>
2023-11-30 17:24:41 -0500

    made walls of room

--- main.js
+++ main.js
@@ -1,24 +1,92 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+camera.position.setZ(100);
+camera.position.setY(0);
+
+renderer.render(scene,camera);
+//starting point
+const startingPoint = new THREE.BoxGeometry(10,1,10);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const shape = new THREE.Mesh(startingPoint,material);
+shape.position.setY(-19.5);
+shape.position.setZ(85);
+scene.add(shape);
+//ceiling
+const top = new THREE.BoxGeometry(200,1,200);
+const material2 = new THREE.MeshStandardMaterial({color:0xffffff});
+const ceiling = new THREE.Mesh(top, material2);
+ceiling.position.setY(30);
+ceiling.position.setZ(0);
+scene.add(ceiling);
+//left wall
+const left = new THREE.BoxGeometry(1,50,200);
+const material3 = new THREE.MeshStandardMaterial({color:0xffffff});
+const leftWall = new THREE.Mesh(left, material3);
+leftWall.position.setY(5);
+leftWall.position.setX(100);
+leftWall.position.setZ(0);
+scene.add(leftWall);
+//right wall
+const right = new THREE.BoxGeometry(1,50,200);
+const material4 = new THREE.MeshStandardMaterial({color:0xffffff});
+const rightWall = new THREE.Mesh(right, material4);
+rightWall.position.setY(5);
+rightWall.position.setX(-100);
+rightWall.position.setZ(0);
+scene.add(rightWall);
+//back wall
+const back = new THREE.BoxGeometry(200,50,1);
+const material5 = new THREE.MeshStandardMaterial({color:0xffffff});
+const backWall = new THREE.Mesh(back, material5);
+backWall.position.setY(5);
+backWall.position.setX(0);
+backWall.position.setZ(-100);
+scene.add(backWall);
+//front wall
+const front = new THREE.BoxGeometry(200,50,1);
+const material6 = new THREE.MeshStandardMaterial({color:0xffffff});
+const frontWall = new THREE.Mesh(front, material6);
+frontWall.position.setY(5);
+frontWall.position.setX(0);
+frontWall.position.setZ(100);
+scene.add(frontWall);
+
+//light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+//grid helper
+const gridHelper = new THREE.GridHelper(200,50);
+gridHelper.position.setY(0);
+gridHelper.position.setZ(0);
+scene.add(gridHelper);
+
+const gridHelper2 = new THREE.GridHelper(200,50);
+gridHelper2.position.setY(-20);
+gridHelper2.position.setZ(0);
+scene.add(gridHelper2);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate(){
+  requestAnimationFrame(animate);
+  /*shape.rotation.x += 0.01;
+  shape.rotation.y += 0.005;
+  shape.rotation.z += 0.01;*/
+  //controls.update();
+  renderer.render(scene,camera);
+}
+
+animate();
